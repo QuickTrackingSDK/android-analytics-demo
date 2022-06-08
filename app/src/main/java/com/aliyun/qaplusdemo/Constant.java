1565915ecd9bd2d899d2efb0c9d55ea2b7a91519
yujie <yj131525@umeng.com>
2022-06-08 14:16:26 +0800

    1.更新

--- app/src/main/java/com/aliyun/qaplusdemo/Constant.java
+++ app/src/main/java/com/aliyun/qaplusdemo/Constant.java
@@ -5,9 +5,7 @@ public class Constant {
     public static final String IMAGE_2 = "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3778456200,3076998411&fm=23&gp=0.jpg";
     public static final String IMAGE_3 = "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1017904219,2460650030&fm=23&gp=0.jpg";
 
-    public static final String DEFAULT_APPKEY = "64632267";
+    public static final String DEFAULT_APPKEY = "您的appkey";
     public static final String DEFAULT_CHANNEL = "Aliyun";
-    public static final String DEFAULT_HOST = "https://log-api.aplus.emas-poc.com";
-    // 日常收数地址
-    //public static final String DEFAULT_HOST = "https://log-api-daily.aplus.emas-poc.com";
+    public static final String DEFAULT_HOST = "https://您的收数域名";
 }
